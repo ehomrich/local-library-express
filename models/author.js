d4b58d14c04ec78e81f3d222b8c88e04cfd9c15f
Émerson Pinto Homrich <ehomrich@users.noreply.github.com>
2018-07-24 18:00:50 -0300

    Add book, author, genre and book instance models

---
+++ models/author.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const AuthorSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    family_name: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date }
+});
+
+AuthorSchema.virtual('name').get(() => {
+    return `${this.family_name}, ${this.first_name}`;
+});
+
+AuthorSchema.virtual('url').get(() => {
+    return `/catalog/author/${this._id}`;
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
