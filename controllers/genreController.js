d59db87445f6be601ccd3a2d008e302fee9781df
Émerson Pinto Homrich <ehomrich@users.noreply.github.com>
2018-07-25 15:27:50 -0300

    Add genre detail view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 const Genre = require('../models/genre');
+const Book = require('../models/book');
+
+const async = require('async');
 
 exports.list = (req, res, next) => {
     Genre.find()
@@ -13,7 +16,34 @@ exports.list = (req, res, next) => {
 };
 
 exports.detail = (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+    async.parallel(
+        {
+            genre: callback => {
+                Genre.findById(req.params.id).exec(callback);
+            },
+            genreBooks: callback => {
+                Book.find({ genre: req.params.id }).exec(callback);
+            }
+        },
+        (err, results) => {
+            if (err) {
+                return next(err);
+            }
+
+            if (results.genre == null) {
+                const err = new Error('Genre not found');
+                err.status = 404;
+
+                return next(err);
+            }
+
+            res.render('genre_detail', {
+                title: 'Genre detail',
+                genre: results.genre,
+                genreBooks: results.genreBooks
+            });
+        }
+    );
 };
 
 exports.create_get = (req, res, next) => {
