7e7ba9a13ee40ab4993374a617ba2f1ce38a1d51
Émerson Pinto Homrich <ehomrich@users.noreply.github.com>
2018-07-25 14:33:27 -0300

    Add index and book list logic

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,13 +1,49 @@
 const mongoose = require('mongoose');
 
-const Book = mongoose.model('Book');
+const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
+
+const async = require('async');
 
 exports.index = (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+    async.parallel(
+        {
+            bookCount: callback => {
+                Book.countDocuments({}, callback);
+            },
+            bookInstanceCount: callback => {
+                BookInstance.countDocuments({}, callback);
+            },
+            bookInstanceAvailableCount: callback => {
+                BookInstance.countDocuments({ status: 'Available' }, callback);
+            },
+            authorCount: callback => {
+                Author.countDocuments({}, callback);
+            },
+            genreCount: callback => {
+                Genre.countDocuments({}, callback);
+            }
+        },
+        (err, results) => {
+            res.render('index', {
+                title: 'Local Library Home',
+                error: err,
+                data: results
+            });
+        }
+    );
 };
 
 exports.list = (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Book list');
+    Book.find({}, 'title author')
+        .populate('author')
+        .exec((err, booksList) => {
+            if (err) { return next(err); }
+
+            res.render('book_list', { title: 'Book List', booksList: booksList });
+        });
 };
 
 exports.detail = (req, res, next) => {
